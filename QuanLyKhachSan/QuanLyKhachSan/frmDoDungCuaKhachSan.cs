e63e6e9c168293fe819e44b20a1dafd63663c7b8
truongmta190298 <camamidol@gmail.com>
2019-11-26 22:50:18 +0700

    ductruong cellclick

--- QuanLyKhachSan/QuanLyKhachSan/frmDoDungCuaKhachSan.cs
+++ QuanLyKhachSan/QuanLyKhachSan/frmDoDungCuaKhachSan.cs
@@ -27,5 +27,20 @@ namespace QuanLyKhachSan
         {
             dgvDoDung.DataSource = dd.HienThiDoDung2();
         }
+        private void dgvDoDung_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                txtMaDD.Text = dgvDoDung.Rows[e.RowIndex].Cells[0].Value.ToString();
+                txtTenDD.Text = dgvDoDung.Rows[e.RowIndex].Cells[1].Value.ToString();
+                txtSoLuong.Text = dgvDoDung.Rows[e.RowIndex].Cells[2].Value.ToString();
+                txtDonViTinh.Text = dgvDoDung.Rows[e.RowIndex].Cells[3].Value.ToString();
+                txtGiaNhap.Text = dgvDoDung.Rows[e.RowIndex].Cells[4].Value.ToString();
+            }
+            catch
+            {
+
+            }
+        }
     }
 }
