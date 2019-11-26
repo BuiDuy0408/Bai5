de8fc846ba0ce3045dca5265169f43795e3c5130
truong19961405 <54049414+truong19961405@users.noreply.github.com>
2019-11-26 22:38:26 +0700

    xtruong
    xtruong_image

--- QuanLyKhachSan/QuanLyKhachSan/Form1.Designer.cs
+++ QuanLyKhachSan/QuanLyKhachSan/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.groupBox5 = new System.Windows.Forms.GroupBox();
             this.groupBox4 = new System.Windows.Forms.GroupBox();
             this.btnLuu = new System.Windows.Forms.Button();
@@ -47,8 +48,12 @@
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.txttk_loaiphong = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txttk_tenphong = new System.Windows.Forms.TextBox();
             this.groupBox5.SuspendLayout();
             this.groupBox4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // groupBox5
@@ -71,7 +76,7 @@
             this.groupBox5.Margin = new System.Windows.Forms.Padding(4);
             this.groupBox5.Name = "groupBox5";
             this.groupBox5.Padding = new System.Windows.Forms.Padding(4);
-            this.groupBox5.Size = new System.Drawing.Size(1081, 527);
+            this.groupBox5.Size = new System.Drawing.Size(1229, 527);
             this.groupBox5.TabIndex = 56;
             this.groupBox5.TabStop = false;
             this.groupBox5.Enter += new System.EventHandler(this.groupBox5_Enter);
@@ -79,6 +84,9 @@
             // groupBox4
             // 
             this.groupBox4.BackColor = System.Drawing.Color.Khaki;
+            this.groupBox4.Controls.Add(this.txttk_tenphong);
+            this.groupBox4.Controls.Add(this.label7);
+            this.groupBox4.Controls.Add(this.pictureBox1);
             this.groupBox4.Controls.Add(this.txttk_loaiphong);
             this.groupBox4.Controls.Add(this.btnXoa);
             this.groupBox4.Controls.Add(this.btnHuy);
@@ -86,17 +94,17 @@
             this.groupBox4.Controls.Add(this.btnLuu);
             this.groupBox4.Controls.Add(this.btnThem);
             this.groupBox4.Dock = System.Windows.Forms.DockStyle.Right;
-            this.groupBox4.Location = new System.Drawing.Point(593, 19);
+            this.groupBox4.Location = new System.Drawing.Point(618, 19);
             this.groupBox4.Margin = new System.Windows.Forms.Padding(4);
             this.groupBox4.Name = "groupBox4";
             this.groupBox4.Padding = new System.Windows.Forms.Padding(4);
-            this.groupBox4.Size = new System.Drawing.Size(484, 504);
+            this.groupBox4.Size = new System.Drawing.Size(607, 504);
             this.groupBox4.TabIndex = 62;
             this.groupBox4.TabStop = false;
             // 
             // btnLuu
             // 
-            this.btnLuu.Location = new System.Drawing.Point(251, 5);
+            this.btnLuu.Location = new System.Drawing.Point(134, 13);
             this.btnLuu.Margin = new System.Windows.Forms.Padding(4);
             this.btnLuu.Name = "btnLuu";
             this.btnLuu.Size = new System.Drawing.Size(100, 43);
@@ -106,7 +114,7 @@
             // 
             // btnThem
             // 
-            this.btnThem.Location = new System.Drawing.Point(116, 5);
+            this.btnThem.Location = new System.Drawing.Point(8, 13);
             this.btnThem.Margin = new System.Windows.Forms.Padding(4);
             this.btnThem.Name = "btnThem";
             this.btnThem.Size = new System.Drawing.Size(100, 43);
@@ -181,7 +189,7 @@
             // 
             // btnSua
             // 
-            this.btnSua.Location = new System.Drawing.Point(116, 79);
+            this.btnSua.Location = new System.Drawing.Point(8, 70);
             this.btnSua.Margin = new System.Windows.Forms.Padding(4);
             this.btnSua.Name = "btnSua";
             this.btnSua.Size = new System.Drawing.Size(100, 43);
@@ -191,7 +199,7 @@
             // 
             // btnHuy
             // 
-            this.btnHuy.Location = new System.Drawing.Point(251, 79);
+            this.btnHuy.Location = new System.Drawing.Point(134, 70);
             this.btnHuy.Margin = new System.Windows.Forms.Padding(4);
             this.btnHuy.Name = "btnHuy";
             this.btnHuy.Size = new System.Drawing.Size(100, 43);
@@ -201,7 +209,7 @@
             // 
             // btnXoa
             // 
-            this.btnXoa.Location = new System.Drawing.Point(251, 140);
+            this.btnXoa.Location = new System.Drawing.Point(8, 121);
             this.btnXoa.Margin = new System.Windows.Forms.Padding(4);
             this.btnXoa.Name = "btnXoa";
             this.btnXoa.Size = new System.Drawing.Size(100, 43);
@@ -255,17 +263,47 @@
             this.txttk_loaiphong.Size = new System.Drawing.Size(180, 22);
             this.txttk_loaiphong.TabIndex = 60;
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(274, -6);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(337, 291);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 61;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(242, 316);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(77, 17);
+            this.label7.TabIndex = 62;
+            this.label7.Text = "Tên phòng";
+            // 
+            // txttk_tenphong
+            // 
+            this.txttk_tenphong.Location = new System.Drawing.Point(355, 315);
+            this.txttk_tenphong.Margin = new System.Windows.Forms.Padding(4);
+            this.txttk_tenphong.Name = "txttk_tenphong";
+            this.txttk_tenphong.Size = new System.Drawing.Size(180, 22);
+            this.txttk_tenphong.TabIndex = 63;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1081, 527);
+            this.ClientSize = new System.Drawing.Size(1229, 527);
             this.Controls.Add(this.groupBox5);
             this.Name = "Form1";
             this.groupBox5.ResumeLayout(false);
             this.groupBox5.PerformLayout();
             this.groupBox4.ResumeLayout(false);
             this.groupBox4.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -291,5 +329,8 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox txttk_loaiphong;
+        private System.Windows.Forms.TextBox txttk_tenphong;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
