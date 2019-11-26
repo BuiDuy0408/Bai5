d3dd2c40f8859bcf90c205516656358f5a0fe82f
hnahtit <ngtrungthanhcntt2@gmail.com>
2019-11-26 15:23:49 +0700

    thanh-add them khach

--- QuanLyKhachSan/QuanLyKhachSan/frmChoThuePhong.Designer.cs
+++ QuanLyKhachSan/QuanLyKhachSan/frmChoThuePhong.Designer.cs
@@ -28,10 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnThemKhachMoi = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
-            this.txtMaKH = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
             this.btnOK = new System.Windows.Forms.Button();
             this.btnThemDV = new System.Windows.Forms.Button();
             this.txtMaDV = new System.Windows.Forms.TextBox();
@@ -39,50 +35,17 @@
             this.btnChonPhong = new System.Windows.Forms.Button();
             this.txtMaPhong = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.dgvDichVu = new System.Windows.Forms.DataGridView();
-            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column10 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            ((System.ComponentModel.ISupportInitialize)(this.dgvDichVu)).BeginInit();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnThemKhachMoi = new System.Windows.Forms.Button();
+            this.dgvKhachThuePhong = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.txtMaKH = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvKhachThuePhong)).BeginInit();
             this.SuspendLayout();
-            // 
-            // btnThemKhachMoi
-            // 
-            this.btnThemKhachMoi.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnThemKhachMoi.Location = new System.Drawing.Point(40, 88);
-            this.btnThemKhachMoi.Name = "btnThemKhachMoi";
-            this.btnThemKhachMoi.Size = new System.Drawing.Size(132, 46);
-            this.btnThemKhachMoi.TabIndex = 4;
-            this.btnThemKhachMoi.Text = "Thêm Khách Mới";
-            this.btnThemKhachMoi.UseVisualStyleBackColor = true;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(193, 68);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 19);
-            this.button1.TabIndex = 8;
-            this.button1.Text = "Refresh";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // txtMaKH
-            // 
-            this.txtMaKH.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtMaKH.Location = new System.Drawing.Point(40, 66);
-            this.txtMaKH.Name = "txtMaKH";
-            this.txtMaKH.Size = new System.Drawing.Size(147, 22);
-            this.txtMaKH.TabIndex = 6;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(40, 47);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(162, 16);
-            this.label1.TabIndex = 5;
-            this.label1.Text = "Mã Khách Thuê Phòng";
             // 
             // btnOK
             // 
@@ -152,51 +115,89 @@
             this.label2.TabIndex = 9;
             this.label2.Text = "Mã Phòng";
             // 
-            // dgvDichVu
+            // panel1
             // 
-            this.dgvDichVu.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.dgvDichVu.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvDichVu.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.Column7,
-            this.Column8,
-            this.Column9,
-            this.Column10});
-            this.dgvDichVu.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.dgvDichVu.Location = new System.Drawing.Point(0, 0);
-            this.dgvDichVu.Name = "dgvDichVu";
-            this.dgvDichVu.Size = new System.Drawing.Size(778, 460);
-            this.dgvDichVu.TabIndex = 16;
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.btnThemKhachMoi);
+            this.panel1.Controls.Add(this.dgvKhachThuePhong);
+            this.panel1.Controls.Add(this.txtMaKH);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(279, 498);
+            this.panel1.TabIndex = 16;
             // 
-            // Column7
+            // button1
             // 
-            this.Column7.DataPropertyName = "MaDV";
-            this.Column7.HeaderText = "Mã DV";
-            this.Column7.Name = "Column7";
+            this.button1.Location = new System.Drawing.Point(201, 121);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Refresh";
+            this.button1.UseVisualStyleBackColor = true;
             // 
-            // Column8
+            // btnThemKhachMoi
             // 
-            this.Column8.DataPropertyName = "TenDV";
-            this.Column8.HeaderText = "Tên DV";
-            this.Column8.Name = "Column8";
+            this.btnThemKhachMoi.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnThemKhachMoi.Location = new System.Drawing.Point(55, 29);
+            this.btnThemKhachMoi.Name = "btnThemKhachMoi";
+            this.btnThemKhachMoi.Size = new System.Drawing.Size(132, 46);
+            this.btnThemKhachMoi.TabIndex = 3;
+            this.btnThemKhachMoi.Text = "Thêm Khách Mới";
+            this.btnThemKhachMoi.UseVisualStyleBackColor = true;
             // 
-            // Column9
+            // dgvKhachThuePhong
             // 
-            this.Column9.DataPropertyName = "Gia";
-            this.Column9.HeaderText = "Giá";
-            this.Column9.Name = "Column9";
+            this.dgvKhachThuePhong.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.dgvKhachThuePhong.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvKhachThuePhong.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2});
+            this.dgvKhachThuePhong.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.dgvKhachThuePhong.Location = new System.Drawing.Point(0, 174);
+            this.dgvKhachThuePhong.Name = "dgvKhachThuePhong";
+            this.dgvKhachThuePhong.Size = new System.Drawing.Size(279, 324);
+            this.dgvKhachThuePhong.TabIndex = 2;
             // 
-            // Column10
+            // Column1
             // 
-            this.Column10.DataPropertyName = "DonViTinh";
-            this.Column10.HeaderText = "Đơn Vị Tính";
-            this.Column10.Name = "Column10";
+            this.Column1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column1.DataPropertyName = "MaKH";
+            this.Column1.HeaderText = "Mã KH";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column2.DataPropertyName = "TenKH";
+            this.Column2.HeaderText = "Tên Khách";
+            this.Column2.Name = "Column2";
+            // 
+            // txtMaKH
+            // 
+            this.txtMaKH.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMaKH.Location = new System.Drawing.Point(40, 121);
+            this.txtMaKH.Name = "txtMaKH";
+            this.txtMaKH.Size = new System.Drawing.Size(147, 22);
+            this.txtMaKH.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(40, 94);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(162, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Mã Khách Thuê Phòng";
             // 
             // frmChoThuePhong
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(778, 460);
-            this.Controls.Add(this.dgvDichVu);
+            this.ClientSize = new System.Drawing.Size(778, 498);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.btnOK);
             this.Controls.Add(this.btnThemDV);
             this.Controls.Add(this.txtMaDV);
@@ -204,24 +205,17 @@
             this.Controls.Add(this.btnChonPhong);
             this.Controls.Add(this.txtMaPhong);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.txtMaKH);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.btnThemKhachMoi);
             this.Name = "frmChoThuePhong";
             this.Text = "frmChoThuePhong";
-            ((System.ComponentModel.ISupportInitialize)(this.dgvDichVu)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvKhachThuePhong)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button btnThemKhachMoi;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox txtMaKH;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button btnOK;
         private System.Windows.Forms.Button btnThemDV;
         private System.Windows.Forms.TextBox txtMaDV;
@@ -229,10 +223,13 @@
         private System.Windows.Forms.Button btnChonPhong;
         private System.Windows.Forms.TextBox txtMaPhong;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.DataGridView dgvDichVu;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column8;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column9;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column10;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnThemKhachMoi;
+        private System.Windows.Forms.DataGridView dgvKhachThuePhong;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.TextBox txtMaKH;
+        private System.Windows.Forms.Label label1;
     }
 }
