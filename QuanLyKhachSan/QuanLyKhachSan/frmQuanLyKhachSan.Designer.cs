c75dfcbe085e3728cfdd2783c4cf8001582e8e97
LuThaiHoc <50101866+LuThaiHoc@users.noreply.github.com>
2019-11-27 09:10:32 +0700

    frmQLKS
    xoa menu trip

--- QuanLyKhachSan/QuanLyKhachSan/frmQuanLyKhachSan.Designer.cs
+++ QuanLyKhachSan/QuanLyKhachSan/frmQuanLyKhachSan.Designer.cs
@@ -29,103 +29,9 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmQuanLyKhachSan));
-            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-            this.quảnLýToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.quảnLýPhòngToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.quảnLýĐồDùngToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.quảnLýKháchThuêToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
-            this.thoátToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.hoạtĐộngToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.thuêPhòngToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.trảPhòngToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.hướngDẫnToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.pictureBoxBackground = new System.Windows.Forms.PictureBox();
-            this.menuStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxBackground)).BeginInit();
             this.SuspendLayout();
-            // 
-            // menuStrip1
-            // 
-            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
-            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.quảnLýToolStripMenuItem,
-            this.hoạtĐộngToolStripMenuItem,
-            this.hướngDẫnToolStripMenuItem});
-            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
-            this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Padding = new System.Windows.Forms.Padding(8, 2, 0, 2);
-            this.menuStrip1.Size = new System.Drawing.Size(941, 28);
-            this.menuStrip1.TabIndex = 1;
-            this.menuStrip1.Text = "menuStrip1";
-            // 
-            // quảnLýToolStripMenuItem
-            // 
-            this.quảnLýToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.quảnLýPhòngToolStripMenuItem,
-            this.quảnLýĐồDùngToolStripMenuItem,
-            this.quảnLýKháchThuêToolStripMenuItem,
-            this.toolStripMenuItem1,
-            this.thoátToolStripMenuItem});
-            this.quảnLýToolStripMenuItem.Name = "quảnLýToolStripMenuItem";
-            this.quảnLýToolStripMenuItem.Size = new System.Drawing.Size(73, 24);
-            this.quảnLýToolStripMenuItem.Text = "Quản Lý";
-            // 
-            // quảnLýPhòngToolStripMenuItem
-            // 
-            this.quảnLýPhòngToolStripMenuItem.Name = "quảnLýPhòngToolStripMenuItem";
-            this.quảnLýPhòngToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
-            this.quảnLýPhòngToolStripMenuItem.Text = "Quản Lý Phòng";
-            // 
-            // quảnLýĐồDùngToolStripMenuItem
-            // 
-            this.quảnLýĐồDùngToolStripMenuItem.Name = "quảnLýĐồDùngToolStripMenuItem";
-            this.quảnLýĐồDùngToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
-            this.quảnLýĐồDùngToolStripMenuItem.Text = "Quản Lý Đồ Dùng";
-            // 
-            // quảnLýKháchThuêToolStripMenuItem
-            // 
-            this.quảnLýKháchThuêToolStripMenuItem.Name = "quảnLýKháchThuêToolStripMenuItem";
-            this.quảnLýKháchThuêToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
-            this.quảnLýKháchThuêToolStripMenuItem.Text = "Quản Lý Khách Thuê";
-            // 
-            // toolStripMenuItem1
-            // 
-            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
-            this.toolStripMenuItem1.Size = new System.Drawing.Size(213, 6);
-            // 
-            // thoátToolStripMenuItem
-            // 
-            this.thoátToolStripMenuItem.Name = "thoátToolStripMenuItem";
-            this.thoátToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
-            this.thoátToolStripMenuItem.Text = "Thoát";
-            // 
-            // hoạtĐộngToolStripMenuItem
-            // 
-            this.hoạtĐộngToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.thuêPhòngToolStripMenuItem,
-            this.trảPhòngToolStripMenuItem});
-            this.hoạtĐộngToolStripMenuItem.Name = "hoạtĐộngToolStripMenuItem";
-            this.hoạtĐộngToolStripMenuItem.Size = new System.Drawing.Size(95, 24);
-            this.hoạtĐộngToolStripMenuItem.Text = "Hoạt Động";
-            // 
-            // thuêPhòngToolStripMenuItem
-            // 
-            this.thuêPhòngToolStripMenuItem.Name = "thuêPhòngToolStripMenuItem";
-            this.thuêPhòngToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
-            this.thuêPhòngToolStripMenuItem.Text = "Thuê Phòng";
-            // 
-            // trảPhòngToolStripMenuItem
-            // 
-            this.trảPhòngToolStripMenuItem.Name = "trảPhòngToolStripMenuItem";
-            this.trảPhòngToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
-            this.trảPhòngToolStripMenuItem.Text = "Trả Phòng";
-            // 
-            // hướngDẫnToolStripMenuItem
-            // 
-            this.hướngDẫnToolStripMenuItem.Name = "hướngDẫnToolStripMenuItem";
-            this.hướngDẫnToolStripMenuItem.Size = new System.Drawing.Size(98, 24);
-            this.hướngDẫnToolStripMenuItem.Text = "Hướng Dẫn";
             // 
             // pictureBoxBackground
             // 
@@ -133,10 +39,10 @@
             this.pictureBoxBackground.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.pictureBoxBackground.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.pictureBoxBackground.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pictureBoxBackground.Location = new System.Drawing.Point(0, 28);
+            this.pictureBoxBackground.Location = new System.Drawing.Point(0, 0);
             this.pictureBoxBackground.Margin = new System.Windows.Forms.Padding(4);
             this.pictureBoxBackground.Name = "pictureBoxBackground";
-            this.pictureBoxBackground.Size = new System.Drawing.Size(941, 501);
+            this.pictureBoxBackground.Size = new System.Drawing.Size(941, 529);
             this.pictureBoxBackground.TabIndex = 2;
             this.pictureBoxBackground.TabStop = false;
             // 
@@ -146,30 +52,14 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(941, 529);
             this.Controls.Add(this.pictureBoxBackground);
-            this.Controls.Add(this.menuStrip1);
             this.Name = "frmQuanLyKhachSan";
             this.Text = "Quản Lý Khách Sạn";
-            this.menuStrip1.ResumeLayout(false);
-            this.menuStrip1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxBackground)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.MenuStrip menuStrip1;
-        private System.Windows.Forms.ToolStripMenuItem quảnLýToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem quảnLýPhòngToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem quảnLýĐồDùngToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem quảnLýKháchThuêToolStripMenuItem;
-        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
-        private System.Windows.Forms.ToolStripMenuItem thoátToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem hoạtĐộngToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem thuêPhòngToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem trảPhòngToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem hướngDẫnToolStripMenuItem;
         private System.Windows.Forms.PictureBox pictureBoxBackground;
     }
 }
